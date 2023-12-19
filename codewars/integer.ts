f36848ecf223576844f34b4c04ac345d72a9e4c0
sarawut437 <sarawutohm437@gmail.com>
2023-12-19 13:43:25 +0700

    feat: :sparkles: add integer

---
+++ codewars/integer.ts
@@ -0,0 +1,20 @@
+function isSquare(n: number): boolean {
+
+    if (n >= 0) {
+        
+        const squareRoot = Math.sqrt(n);
+        
+        return Number.isInteger(squareRoot);
+    } else {
+        return false; 
+    }
+}
+
+console.log(isSquare(-1));  // Output: false
+console.log(isSquare(0));   // Output: true
+console.log(isSquare(3));   // Output: false
+console.log(isSquare(4));   // Output: true
+console.log(isSquare(25));  // Output: true
+console.log(isSquare(26));  // Output: false
+
+
